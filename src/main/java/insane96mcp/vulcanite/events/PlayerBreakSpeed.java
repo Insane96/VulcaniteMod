7601aa56c464d69dea72033a8e18a85b65042570
Insane96 <insane96mcp@gmail.com>
2020-04-03 19:54:12 +0200

    Check for nether efficiency is now done correctly for checking if the tool is effective

--- src/main/java/insane96mcp/vulcanite/events/PlayerBreakSpeed.java
+++ src/main/java/insane96mcp/vulcanite/events/PlayerBreakSpeed.java
@@ -3,12 +3,19 @@ package insane96mcp.vulcanite.events;
 
 import insane96mcp.vulcanite.Vulcanite;
 import insane96mcp.vulcanite.setup.ModConfig;
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.Set;
 
 @Mod.EventBusSubscriber(modid = Vulcanite.MOD_ID)
 public class PlayerBreakSpeed {
@@ -16,14 +23,23 @@ public class PlayerBreakSpeed {
 	@SubscribeEvent
 	public static void playerBreakSpeedEvent(PlayerEvent.BreakSpeed event) {
 		PlayerEntity player = event.getPlayer();
-
 		if (player.dimension.getId() != -1)
 			return;
-
-		ItemStack mainHand = player.getHeldItemMainhand();
-
-		if (mainHand.getItem().getTags().contains(new ResourceLocation(Vulcanite.MOD_ID, "more_efficient_tools"))
-				&& mainHand.getItem().getDestroySpeed(mainHand, event.getState()) > 1.0f) {
+		ItemStack heldStack = player.getHeldItemMainhand();
+		Block block = event.getState().getBlock();
+		Set<ToolType> types = heldStack.getItem().getToolTypes(heldStack);
+		boolean isToolEffective = false;
+		for (ToolType toolType : types) {
+			if (block.isToolEffective(event.getState(), toolType)) {
+				isToolEffective = true;
+				break;
+			}
+		}
+		Tag<Item> moreEfficientTools = ItemTags.getCollection().get(new ResourceLocation(Vulcanite.MOD_ID, "more_efficient_tools"));
+		if (moreEfficientTools == null)
+			return;
+		boolean isInTag = heldStack.getItem().isIn(moreEfficientTools);
+		if (isInTag && isToolEffective) {
 			float speed = event.getOriginalSpeed();
 			speed += event.getOriginalSpeed() * ModConfig.COMMON.toolsAndWeapons.bonusStats.efficiency.get() / 100f;
 			event.setNewSpeed(speed);
